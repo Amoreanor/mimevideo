23070cb2f98a3f6b1c9ce78939c3d405e052fdf9
AlexanderMoreR <alexandermoreanor@gmail.com>
2020-09-19 14:23:04 -0500

    video js npm inplementado

--- Frontend/src/app/pages/post-preview/post-preview.component.ts
+++ Frontend/src/app/pages/post-preview/post-preview.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import videojs from 'video.js';
+import { Component, ElementRef, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ViewEncapsulation } from '@angular/compiler/src/core';
+import { ViewEncapsulation } from '@angular/core';
 import { PostService } from '../../services/post.service';
 
 import { Post } from '../../interfaces/Post';
@@ -11,10 +12,22 @@ import { Post } from '../../interfaces/Post';
   styleUrls: ['./post-preview.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class PostPreviewComponent implements OnInit {
+export class PostPreviewComponent implements OnDestroy, AfterViewInit {
+  @ViewChild('target', { static: false }) target: ElementRef;
 
   id: string;
   post: Post;
+  options: {
+    fluid: boolean,
+    aspectRatio: string,
+    autoplay: boolean,
+    sources: {
+        src: string,
+        type: string,
+    }[],
+  };
+
+  player: videojs.Player;
 
   constructor(
     private activeRoute: ActivatedRoute,
@@ -22,17 +35,39 @@ export class PostPreviewComponent implements OnInit {
     private postService: PostService
   ) { }
 
-  ngOnInit() {
+  ngAfterViewInit() {
+    const video = window.document.getElementById('target');
+    console.log(this.target);
+    console.log(video);
     this.activeRoute.params.subscribe(params =>{
       this.id = params['id'];
       this.postService.getPost(this.id)
           .subscribe(
           res => {
             this.post = res;
+            this.options = {
+              fluid: true,
+              aspectRatio: '16:9',
+              autoplay: false,
+              sources: [{
+                  src: 'http://localhost:3000/'+ this.post.url,
+                  type: 'video/mp4',
+              }]
+            };
+            this.player = videojs(this.target.nativeElement, this.options, () => {
+              console.log('onPlayerReady', this);
+            });
           },
           error => console.log(error)
           )
     });
+  }
+
+  ngOnDestroy() {
+    // destroy player - elimina el proceso del DOM del this.player
+    if (this.player) {
+      this.player.dispose();
+    }
   }
 
   deletePost(id: string){
